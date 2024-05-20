cbfec75f3d1567c83cb28eb98080010e705e7265
Zwanzen <47717833+Zwanzen@users.noreply.github.com>
2024-05-20 21:16:07 +0200

    O2 Almost Finished

--- Assets/Scripts/O2Controller.cs
+++ Assets/Scripts/O2Controller.cs
@@ -1,33 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class O2Controller : MonoBehaviour
 {
+    [SerializeField] private UnityEvent endEvent;
 
     [SerializeField] private MeshRenderer[] meshRenderers;
-    [SerializeField] private Material redMaterial;
+    private Material notFixedMaterial;
     [SerializeField] private Material fixedMaterial;
+
+    private bool isFixed = false;
+    private int blinkCount = 0;
 
     private void Start()
     {
-        foreach (MeshRenderer meshRenderer in meshRenderers)
-        {
-            meshRenderer.material = redMaterial;
-
-        }
+        notFixedMaterial = meshRenderers[0].material;
     }
 
     public void FixO2()
     {
-        ChangeMaterials();
+        isFixed = true;
     }
 
-    private void ChangeMaterials()
+    private void Update()
+    {
+        if (isFixed)
+        {
+            // Make the material blink before changing it
+            StartCoroutine(BlinkMaterial());
+            isFixed = false;
+        }
+    }
+
+    private IEnumerator BlinkMaterial()
+    {
+        while (blinkCount < 3)
+        {
+            ChangeMaterials(fixedMaterial);
+            yield return new WaitForSeconds(0.25f);
+            ChangeMaterials(notFixedMaterial);
+            yield return new WaitForSeconds(0.25f);
+            blinkCount++;
+        }
+        ChangeMaterials(fixedMaterial);
+        endEvent.Invoke();
+    }
+
+    private void ChangeMaterials(Material m)
     {
         foreach (MeshRenderer meshRenderer in meshRenderers)
         {
-            meshRenderer.material = fixedMaterial;
+            meshRenderer.material = m;
         }
     }
 
